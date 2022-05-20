0f9ad43d25ae9606968a3b1211b1c916540b60fd
atsumi <atsumi.amaya@tecsup.edu.pe>
2022-05-19 23:59:22 -0500

    Agregar archivos de proyecto.

---
+++ ejercicio10/ejercicio10/App.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace ejercicio10
+{
+    public partial class App : Application
+    {
+        public static float ScreenHeight { get; set; }
+        public static float ScreenWidth { get; set; }
+
+        public App()
+        {
+            InitializeComponent();
+
+            //MainPage = new MainPage();
+            //MainPage = new Tap();
+            //MainPage = new Pinch();
+            //MainPage = new Pan();
+            MainPage = new Swipe();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
